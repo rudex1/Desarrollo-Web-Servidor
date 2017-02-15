1e4b37c31c659a96e4f1e260e2ff9c1c72f68d65
rudex1 <edwinrudysejasarze@gmail.com>
2017-02-15 09:02:06 +0000

    15/02/2017-10:00:00-rudy

---
+++ KantxApp-master/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('user/activation/{token}', 'Auth\LoginController@activateUser')->name('user.activate');
+
+Route::get('/redirect/{provider}', 'SocialAuthController@redirect');
+Route::get('/callback/{provider}', 'SocialAuthController@callback');
+
+Route::get('settings/account', 'AccountsController@edit');
+Route::patch('settings/account', 'AccountsController@update');
+
+Route::post('/contacto', 'ContactController@sendContact');
